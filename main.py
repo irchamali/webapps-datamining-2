dacb2721cb743c5d1d0bac0a30ebd88b278c6e99
irchamali <id.irchamali@gmail.com>
2022-07-19 00:42:52 +0700

    Modify main.py for webapps using streamlit

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
 import numpy as np
+from PIL import Image
+import pickle
+from sklearn.naive_bayes import GaussianNB
 
-st.title('Klasifikasi Penguin menggunakan ALgoritma Naive Bayes')
+# st.title('Klasifikasi Penguin menggunakan ALgoritma Naive Bayes')
+st.write("""
+# Klasifikasi penguin (Web Apps)
+Aplikasi berbasis Web untuk memprediksi (mengklasifikasi) jenis penguin **Palmer Penguin**
+Data yang didapat dari [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) dalam bentuk R oleh Allison Horst.
+""")
